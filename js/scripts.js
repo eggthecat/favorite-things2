9f63fb4044b9336fba88bfac7b21f417b11ecf68
dominic montelongo and sam cook <dominic@>
2019-06-10 11:37:02 -0700

    create many ways to use arrays

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  //whenever our "form" is "submitted" we perform a "function"
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    // var array = [$("input:text[name=q1]").val(), $("input:text[name=q1]").val(), $("input:text[name=q1]").val()];
+    //
+    // for (num = 0; num < 3; num++) {
+    //   $("ul#list li:nth-child(" + (num + 1) + ")").text(array[num]);
+    // }
+    //
+    //
+    //
+    //
+    //
+    // var question1 = $("input:text[name=q1]").val();
+    // array[0] = question1
+    // alert(array[0]);
+    //
+    //
+    //
+    // array[0] = $("input:text[name=q1]").val();
+
+    //declare array
+    var array = []
+    //This will take the user input and put it in the array
+    array.push($("input:text[name=q1]").val());
+    array.push($("input:text[name=q2]").val());
+    array.push($("input:text[name=q3]").val());
+
+    // This calls out the "#list" to show the input as output in listed items for the array.
+    // this takes the the user input stored in array and outputs it as text to the child of the ul with id list
+    $("ul#list li:nth-child(1)").text(array[0]);
+    $("ul#list li:nth-child(2)").text(array[1]);
+    $("ul#list li:nth-child(3)").text(array[2]);
+
+    console.log(array[0]);
+  });
+});
